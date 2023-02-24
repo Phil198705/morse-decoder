ba4fa4521db0a6e19d0ae864d96cde8466d36758
Phil198705 <Arturik2009phil@mail.ru>
2023-02-24 10:32:40 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let strSize = 2;
+    let res = '';
+    let count = 0;
+    let countLetter = 0;
+    for (let i = 0; i < expr.length; i += strSize){
+      if(expr.slice(i, i + strSize) === '10'){
+        res += '.';
+        countLetter += 1;
+        if(countLetter === 5){
+          res += ' ';
+          countLetter = 0;
+        }  
+      }else if (expr.slice(i, i + strSize) === '11'){
+        res += '-';
+        countLetter += 1;
+        if(countLetter === 5){
+          res += ' ';
+          countLetter = 0;
+        }  
+      }else if (expr.slice(i, i + strSize) === '**'){
+        count += 1;
+        if (count === 5){
+          res += '   ';
+          count = 0;
+        }
+      }else{
+        countLetter += 1;
+        if(countLetter === 5){
+          res += ' ';
+          countLetter = 0;
+        }  
+        continue;
+      }
+    }
+    return res.split('   ').map(el => el.split(' ').map(i => MORSE_TABLE[i]).join('')).join(' ');
+  }
 
 module.exports = {
     decode
